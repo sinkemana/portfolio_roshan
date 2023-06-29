2895620bd34b9db1e6625d5f012d888d51bac0fa
Roshan Sinkemana <sinkemanaroshan5@gmail.com>
2023-06-29 18:44:09 +0545

    portfolio worked on styling thought page

--- src/components/navigation/navigatingItems/Thoughts.js
+++ src/components/navigation/navigatingItems/Thoughts.js
@@ -1,6 +1,18 @@
 import { useState } from "react";
 import object from "../../../dataForThoughts/ThoughtsData";
 import { Accordion, Icon } from "semantic-ui-react";
+import styled from 'styled-components';
+
+const ThoughtContent = styled.div`
+   display:flex;
+   flex-direction:column;
+   justify-content: center;
+   align-items: center;
+   padding-top:10px;
+`
+const SignificatList=styled.div`
+margin-top:15px
+`
 const Thoughts = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const handleClick = (e, props) => {
@@ -29,23 +41,26 @@ const Thoughts = () => {
         )
     })
     return (
-        <div>
+        <ThoughtContent>
             <h3>Thought Process</h3>
-            <p>
-                The evolution of human beings, from their origin to the present day,<br />
-                and their advancement towards the future with sophisticated machines and machine languages,
-                has been linked to the thought process.<br />
-                This Human cognition is so Powerful that built super computing engines to direct homo sapiens
-                to next level of civilization.Here are some of the important reasons why the thought process is significant:
+             <div>  The evolution of human beings, from their origin to the present day,and their advancement towards the future with sophisticated machines and machine languages,has been linked to the thought process.</div>
+               <div> This Human cognition is so Powerful that built super computing engines to direct homo sapiens
+                to next level of civilization.</div>Here are some of the important reasons why the thought process is significant:
 
-            </p>
-            <div>
-                {
+            
+            <SignificatList>
+            {
                     array
                 }
-            </div>
 
-        </div>
+            </SignificatList>
+             
+    
+
+
+        </ThoughtContent>
+
+
     )
 }
 
